af02fe3581435beeb30a09c1c7816b7a6b7d9c58
Jon Stanford <jonathan.c.stanford@gmail.com>
2021-04-28 17:10:54 -0500

    Make GoLang page match html version

--- main.go
+++ main.go
@@ -9,25 +9,32 @@ import (
 
 var (
 	domains = map[string]string{
-		"asciirange.com":          "fas fa-bullseye",
-		"cert.ist":                "fas fa-user-lock",
-		"tilltrump.com":           "far fa-calendar-check",
-		"urip.io":                 "fas fa-code",
+		"asciirange.com": "fas fa-bullseye",
+		"cert.ist":       "fas fa-user-lock",
+		"tilltrump.com":  "far fa-calendar-check",
+		"urip.io":        "fas fa-code",
 	}
 	demos = map[string]string{
-		"wasm.go.demos.jbird.dev": "fas fa-user-secret",
+		"https://github.com/JBirdVegas/go-wasm-hello-world":   "Hello World",
+		"https://github.com/JBirdVegas/jbird.dev/tree/golang": "This site",
 	}
 )
 
 const (
 	introText   = "jbird.dev"
 	taglineText = "Stuff and things... Sometimes useful stuff and things"
+	demosText   = "GoLang WASM demos..."
 )
 
-func createIconAndTextLink(domain, fontAwesomeIcon string) Value {
+func createIconAndTextLink(text *string, domain, fontAwesomeIcon string) Value {
 	a := Document.CreateElement("a")
 	a.Set("target", "_blank")
-	a.Set("href", fmt.Sprintf("https://%s", domain))
+
+	if text == nil {
+		a.Set("href", fmt.Sprintf("https://%s", domain))
+	} else {
+		a.Set("href", domain)
+	}
 	aStyle := a.Get("style")
 	aStyle.Set("display", "inline-block")
 	aStyle.Set("padding", "50px")
@@ -36,7 +43,11 @@ func createIconAndTextLink(domain, fontAwesomeIcon string) Value {
 	i := Document.CreateElement("i")
 	i.Set("className", fontAwesomeIcon)
 	span := Document.CreateElement("span")
-	span.Set("textContent", domain)
+	if text == nil {
+		span.Set("textContent", domain)
+	} else {
+		span.Set("textContent", *text)
+	}
 	span.Get("style").Set("display", "block")
 
 	iStyle := i.Get("style")
@@ -77,10 +88,10 @@ func makeTitleDiv() Value {
 	return div
 }
 
-func makeTaglineDiv() Value {
+func makeTaglineDiv(text string) Value {
 	div := Document.CreateElement("div")
 	div.Set("className", "tagline")
-	div.Set("textContent", taglineText)
+	div.Set("textContent", text)
 	style := div.Get("style")
 	style.Set("font-size", "1.5rem")
 	style.Set("font-weight", "100")
@@ -103,17 +114,21 @@ func main() {
 
 	contentIconsDiv := makeContentIconsDiv()
 	for domain, icon := range domains {
-		contentIconsDiv.Call("appendChild", createIconAndTextLink(domain, icon))
+		contentIconsDiv.Call("appendChild", createIconAndTextLink(nil, domain, icon))
 	}
 
 	demoIconsDiv := makeContentIconsDiv()
-	for domain, icon := range demos {
-		demoIconsDiv.Call("appendChild", createIconAndTextLink(domain, icon))
+	for domain, label := range demos {
+		demoIconsDiv.Call("appendChild", createIconAndTextLink(&label, domain, "fas fa-user-secret"))
 	}
 
 	mainDiv := Document.GetElementById("main")
+	demoDiv := makeTaglineDiv(demosText)
+	demoDiv.Get("style").Set("margin-bottom", "-4%")
+
 	mainDiv.Call("appendChild", makeTitleDiv())
-	mainDiv.Call("appendChild", makeTaglineDiv())
+	mainDiv.Call("appendChild", makeTaglineDiv(taglineText))
 	mainDiv.Call("appendChild", contentIconsDiv)
+	mainDiv.Call("appendChild", demoDiv)
 	mainDiv.Call("appendChild", demoIconsDiv)
 }
